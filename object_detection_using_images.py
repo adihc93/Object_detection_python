6f60541a10168dc3a1712bef7c03e0a7b7608f17
Aditya Chaturvedi <35630730+adihc93@users.noreply.github.com>
2018-04-01 22:20:48 +0530

    Add files via upload

---
+++ object_detection_using_images.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr  1 12:05:23 2018
+
+@author: HP
+"""
+
+import numpy as np
+import cv2
+import tkinter 
+from tkinter.filedialog import askopenfilename
+ 
+CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
+	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+	"sofa", "train", "tvmonitor"]
+COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
+
+print("[INFO] loading model...")
+net = cv2.dnn.readNetFromCaffe("MobileNetSSD_deploy.prototxt.txt", "MobileNetSSD_deploy.caffemodel")
+print("Enter file path: ")
+tkinter.Tk().withdraw()
+filename = askopenfilename()
+image = cv2.imread(filename)
+(h, w) = image.shape[:2]
+blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
+print("[INFO] computing object detections...")
+net.setInput(blob)
+detections = net.forward()
+for i in np.arange(0, detections.shape[2]):
+	# extract the confidence (i.e., probability) associated with the
+	# prediction
+	confidence = detections[0, 0, i, 2]
+ 
+	# filter out weak detections by ensuring the `confidence` is
+	# greater than the minimum confidence
+	if confidence > 0.2:
+		# extract the index of the class label from the `detections`,
+		# then compute the (x, y)-coordinates of the bounding box for
+		# the object
+		idx = int(detections[0, 0, i, 1])
+		box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+		(startX, startY, endX, endY) = box.astype("int")
+ 
+		# display the prediction
+		label = "{}: {:.2f}%".format(CLASSES[idx], confidence * 100)
+		print("[INFO] {}".format(label))
+		cv2.rectangle(image, (startX, startY), (endX, endY),
+			COLORS[idx], 2)
+		y = startY - 15 if startY - 15 > 15 else startY + 15
+		cv2.putText(image, label, (startX, y),
+			cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
+cv2.imshow("Output", image)
+cv2.waitKey(0)
